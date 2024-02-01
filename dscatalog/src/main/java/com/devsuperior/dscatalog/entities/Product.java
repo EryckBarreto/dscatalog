d89c11e70638441acfb92b494d4bb2441e092b0b
Eryck Barreto <eryck.barreto@hotmail.com>
2024-02-01 16:52:29 -0300

    criado product repository, controller, service

--- dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -32,6 +32,9 @@ public class Product {
 
     Set<Category> categories = new HashSet<>();
 
+    public Product() {
+    }
+
     public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
         this.id = id;
         this.name = name;
